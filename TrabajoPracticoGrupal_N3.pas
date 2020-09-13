24c0d1bf278f9ec45d494e963e7389df73bb20aa
Ramiro Di Giacinti <71228261+Ramiro-DG@users.noreply.github.com>
2020-09-13 18:04:10 -0300

    Procedure Sintomas Casi Terminado
    (Falta verificar si existe el codigo)

--- TrabajoPracticoGrupal_N3.pas
+++ TrabajoPracticoGrupal_N3.pas
@@ -15,7 +15,7 @@ Uses CRT,sysutils;
 
 Const
 cant_provincias=24;
-cant_sint=20;
+cant_sint=6;
 cant_enf=10;
 max_sint=6;
 null=char(0)+char(0)+char(0);
@@ -302,7 +302,14 @@ close(APac);
 close(AHist);
 end;
 
-
+Procedure borramela;
+begin
+Rewrite(AProv);
+Rewrite(ASint);
+Rewrite(AEnf);
+Rewrite(APac);
+Rewrite(AHist);
+end;
 
 
 
@@ -479,16 +486,31 @@ end;
 
 
 Procedure Sintomas;     //CARGA DE SINTOMAS
-var a:boolean;
+var
+a:boolean;
+i:integer;
+
 begin
 clrscr;
 a:=true;
+if filesize(ASint)<>0 then
+   begin
+   reset(ASint);
+   Writeln('Sintomas previamente ingresados: ');
+   while not eof(ASint) do
+         begin
+         read(ASint,S);
+         writeln(S.cod,'  ',S.desc);
+         end;
+   writeln('---------------------------------------');
+   end;
+
 if (filesize(ASint)=cant_sint) then writeln('La base de datos esta llena');
-seek(ASint,filesize(ASint));
+
     while (not(filesize(ASint)=cant_sint)) and (a) do
         begin
-            write('Ingrese el codigo del sintoma: ');
-            readln(S.cod);
+            seek(ASint,filesize(ASint));
+            S.cod:=string_num_valido('Ingrese el codigo del sintoma: ',1,3);
             write('Ingrese el nombre del sintoma: ');
             readln(S.desc);
             seek(ASint,filepos(ASint));
@@ -566,8 +588,9 @@ boot;
             writeln('4) Pacientes');
             writeln('5) Historias Clinicas');
             writeln('6) Estadisticas');
+            writeln('7) Borrar datos');
             writeln('0) Fin del Programa');textcolor(7);
-            Opcion:=int_valido('Ingrese la opcion: ',0,6);
+            Opcion:=int_valido('Ingrese la opcion: ',0,7);
             Case Opcion of
             1: Provincias;
             2: Sintomas;
@@ -575,6 +598,7 @@ boot;
             4: Pacientes;
             5: writeln('En construccion');
             6: writeln('En construccion');
+            7: Borramela;
             0: begin
                Andando:=False;
                shutdown;
